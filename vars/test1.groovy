a7c5d23524b1fe858325b2e3c084deadd5824131
Shrey singh <53379309+logisticsshrey001@users.noreply.github.com>
2022-07-14 16:42:47 +0530

    Update test1.groovy

--- vars/test1.groovy
+++ vars/test1.groovy
@@ -1,3 +1,25 @@
 def call(){
-echo 'mvn clean compile'
-}
+stages {
+        stage ('Compile Stage') {
+            steps {
+                 
+                echo 'mvn clean compile'
+                
+            }
+        }
+        stage ('Testing Stage') {
+
+            steps {
+                 
+                    echo 'mvn test'
+                
+            }
+        }
+        stage ('Install Stage') {
+            steps {
+                 
+                    echo 'mvn install'
+                
+            }
+        }
+    }
